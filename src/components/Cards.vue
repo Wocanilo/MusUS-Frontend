77d7f9b1b126698c2f8a6e4ae82ea5b517b8bf17
Wocat <25706428+Wocanilo@users.noreply.github.com>
2020-05-15 19:43:33 +0200

    Better cards for mobile use

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -10,13 +10,14 @@
           :img-alt="item.title"
         >
           <b-card-body>
-            <!-- Picture data-->
+            <!-- Picture data (Big screens)-->
             <div class="row">
-              <div class="col">
-                <h5>{{ item.title }}</h5>
+              <div class="col d-sm-none d-lg-block">
+                <font-awesome-icon icon="calendar"></font-awesome-icon>
+                {{ item.createdAt }}
               </div>
-              <div class="col">
-                <div class="float-lg-right">
+              <div class="col d-sm-none d-lg-block">
+                <div class="float-right">
                   <font-awesome-icon icon="thumbs-up"></font-awesome-icon>
                   {{ item.positive }}
                   <font-awesome-icon icon="thumbs-down"></font-awesome-icon>
@@ -24,17 +25,41 @@
                 </div>
               </div>
             </div>
+            <div class="row">
+              <div class="col d-sm-none d-lg-block mt-1">
+                <h5>{{ item.title }}</h5>
+              </div>
+            </div>
+            <!-- Picture data (Small screens)-->
+            <div class="d-none d-lg-none d-sm-block text-center">
+              <div class="row">
+                <div class="col">
+                  <div class>
+                    <font-awesome-icon icon="thumbs-up"></font-awesome-icon>
+                    {{ item.positive }}
+                    <font-awesome-icon icon="thumbs-down"></font-awesome-icon>
+                    {{ item.negative }}
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <font-awesome-icon icon="calendar"></font-awesome-icon>
+                  {{ item.createdAt }}
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <h5>{{ item.title }}</h5>
+                </div>
+              </div>
+            </div>
+
             <div class="border-top my-4"></div>
             <!-- Horizontal separator -->
-            <b-card-text>
-              {{ item.description }}
-            </b-card-text>
+            <b-card-text>{{ item.description }}</b-card-text>
             <!-- Link to post -->
-            <b-link
-              class="stretched-link"
-              :to="'/post/' + item.id"
-              v-if="userData.isLoggedIn"
-            />
+            <b-link class="stretched-link" :to="'/post/' + item.id" v-if="userData.isLoggedIn" />
           </b-card-body>
         </b-card>
       </b-card-group>
